f3315100674519bdbf0ea4b6f5ba72258a0a9a36
obashanova <oxana.bashanova@gmail.com>
2024-12-02 21:41:44 +0100

    updates

---
+++ src/scripts/index.js
@@ -0,0 +1,9 @@
+// document.addEventListener("astro:page load", () => {
+// 	const aboutSection = document.querySelector("#about");
+// 	const aboutSectionHeight = aboutSection.clientHeight;
+// 	const computedStyle = window.getComputedStyle(aboutSection);
+// 	const paddingTop = parseInt(computedStyle.getPropertyValue("padding-top"));
+
+// 	document.querySelector("#projects").style.scrollMarginTop =
+// 		500 + "px !important";
+// });
